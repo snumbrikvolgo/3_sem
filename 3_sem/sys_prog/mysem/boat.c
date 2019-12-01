feba0e8e5ad82163ea551ce6667ee0691a066b55
Elizaveta Zazhigina <43406459+snumbrikvolgo@users.noreply.github.com>
2019-12-01 23:33:57 +0300

    Add files via upload

--- 3_sem/sys_prog/mysem/boat.c
+++ 3_sem/sys_prog/mysem/boat.c
@@ -10,136 +10,126 @@
 #include <sys/sem.h>
 
 enum sems{
-    SEM_BOAT = 0,
-    SEM_TRAP = 1,
-    SEM_IN   = 2,
-    SEM_EXIT = 3,
+	SEM_BOAT = 0,
+	SEM_TRAP = 1,
+	SEM_IN   = 2,
+	SEM_EXIT = 3,
 };
 
+int check_end(int semid);
+
 #define PLUS(SEM_NAME) \
-    {struct sembuf command = {SEM_NAME, 1, 0}; if (semop(semid, &command,1)==-1){perror("SEM V "); return;}}
+{struct sembuf command = {SEM_NAME, 1, 0}; if (semop(semid, &command,1) < 0){perror("SEM PLUS "); return errno;}}
 #define PLUS_K(SEM_NAME, K) \
-    {struct sembuf command = {SEM_NAME, K, 0}; if(semop(semid, &command, 1)==-1){perror("SEM VK "); return;}}
+{struct sembuf command = {SEM_NAME, K, 0}; if(semop(semid, &command, 1) < 0){perror("SEM PLUS_K "); return errno;}}
 #define MINUS(SEM_NAME) \
-    {struct sembuf command = {SEM_NAME, -1, 0}; if(semop(semid, &command, 1)==-1){perror("SEM P "); return;}}
+{struct sembuf command = {SEM_NAME, -1, 0}; if(semop(semid, &command, 1) < 0){perror("SEM MINUS "); return errno;}}
 #define MINUS_K(SEM_NAME, K) \
-    {struct sembuf command = {SEM_NAME, -K, 0}; if(semop(semid, &command, 1)==-1){perror("SEM PK "); return;}}
+{struct sembuf command = {SEM_NAME, -K, 0}; if(semop(semid, &command, 1) < 0){perror("SEM MINUS_K "); return errno;}}
 #define WAIT(SEM_NAME) \
-    {struct sembuf command = {SEM_NAME, 0, 0}; if(semop(semid, &command, 1)==-1){perror("SEM WAIT "); return;}}
+{struct sembuf command = {SEM_NAME, 0, 0}; if(semop(semid, &command, 1) < 0){perror("SEM WAIT "); return errno;}}
 
-
-    int check(int semid){
-        struct sembuf check = {SEM_EXIT, 0, IPC_NOWAIT};
-        int res = semop(semid, &check, 1);
-        if (res == -1 && errno == EAGAIN) {
-            printf("Break***********************\n");
-            return 1;
-        }
-        else {
-            //my_assert("semop", res != -1);
-            return 0;
-        }
-    }
-
-
-void Passenger(int semid, int i)
+int Passenger(int semid, int i, int numPlace)
 {
-    //struct sembuf command = {-1, 0, 0};
-while(1)
-{
-    printf("Passenger %d wants to get on the boat\n", i);
-    //on_trap
-    if(check(semid)) break;
-    MINUS(SEM_BOAT);
-    if(check(semid)) break;
-    MINUS(SEM_TRAP);
-    if(check(semid)) break;
+	while(1)
+	{
+		printf("Passenger %2d wants to get on the boat\n", i);
+		MINUS(SEM_BOAT);
 
-    PLUS(SEM_TRAP);
-    if(check(semid)) break;
-    printf("Passanger %d is on the boat\n", i);
-    //WAIT(SEM_IN);
-    printf("Passanger %d is in trip\n", i);
-    if(check(semid)) break;
-    printf("Passanger %d leaves the boat\n", i);
-    if(check(semid)) break;
-    MINUS(SEM_TRAP);
-    if(check(semid)) break;
+		if (check_end(semid))
+			break;
 
-    PLUS(SEM_BOAT);
-    if(check(semid)) break;
-    PLUS(SEM_TRAP);
-    if(check(semid)) break;
-    //on boat, wait for everyone
-}
-    PLUS(SEM_BOAT);
-    printf("Passenger %d goes home\n", i);
-    return;
+		MINUS(SEM_TRAP);
+		printf("Passenger %2d: is on trap\n", i);
+		PLUS(SEM_TRAP);
+
+		printf("Passenger %2d: entered the boat\n", i);
+		WAIT(SEM_IN);
+
+		//должен ждать, пока не закончится трип
+		MINUS(SEM_TRAP);
+		PLUS(SEM_BOAT);
+		PLUS(SEM_TRAP);
+
+		printf("Passenger %2d: is again on the beach\n", i);
+	}
+	PLUS(SEM_BOAT);
+	printf("Passenger %2d: leaves the beach\n", i);
+	return 0;
+
 
 }
-void Boat(int semid, int numVoyages, int numPlace)
+int Boat(int semid, int numVoyages, int numPlace)
 {
-    //struct sembuf command = {-1, 0, 0};
-    PLUS_K(SEM_BOAT, numPlace);
-    printf("The boat is ready to transfer\n");
+	printf("Boat: goes to the beach\n");
+	PLUS_K(SEM_BOAT, numPlace);
 
-    for (int i = 1; i <= numVoyages; i++)
-    {
-        printf("The boat is ready to be loaded\n");
+	for (int i = 1; i <= numVoyages; i++)
+	{
+		printf("Boat: ready for trip\n");
+		printf("Boat: opens the ladder\n");
 
-        PLUS(SEM_IN);
-        printf("The boat descends the ladder\n");
-        PLUS(SEM_TRAP);
-        usleep(1000);
-        printf("The boat goes for %d trip\n", i);
-        MINUS(SEM_TRAP);
+		PLUS(SEM_IN);
+		PLUS(SEM_TRAP);
+		usleep(1000);
 
-        MINUS(SEM_IN);
-        usleep(1000);
+		printf("Boat: closes the ladder\n");
 
-    }
+		MINUS(SEM_TRAP);
+		MINUS(SEM_IN);
+		usleep(1000);
+	}
 
-    PLUS(SEM_EXIT);
-    PLUS(SEM_IN);
+	PLUS(SEM_EXIT);
+	PLUS(SEM_IN);
+	PLUS(SEM_TRAP);
+	PLUS_K(SEM_BOAT, numPlace);
 
-    PLUS(SEM_TRAP);
-    PLUS_K(SEM_BOAT, numPlace);
-
-
-    printf("The boat goes home\n");
-
-    return;
+	printf("Boat: finish work\n");
+	return 0;
 }
+
 int main(int argc, char** argv)
 {
-    if (argc != 4) return 0;
+	if (argc != 4) return 0;
 
-    int NumPeople  = atoi(argv[1]);
-    int NumPlace   = atoi(argv[2]);
-    int NumVoyages = atoi(argv[3]);
+	int NumPeople  = atoi(argv[1]);
+	int NumPlace   = atoi(argv[2]);
+	int NumVoyages = atoi(argv[3]);
 
-    int semid = semget(IPC_PRIVATE, 4, 0777);
-    if (semid < 0){
-        perror("Cannot get semaphore id\n");
-        return errno;
-    }
+	int semid = semget(IPC_PRIVATE, 4, 0777);
+	if (semid < 0){
+		perror("Cannot get semaphore id\n");
+		return errno;
+	}
 
-    if (!fork())
-        Boat(semid, NumVoyages, NumPlace);
+	if (!fork())
+		exit(Boat(semid, NumVoyages, NumPlace));
 
-    for (int i = 1; i <= NumPeople; i++){
-        if (!fork())
-            Passenger(semid, i);
-    }
+	for (int i = 1; i <= NumPeople; i++){
+		if (!fork())
+			exit(Passenger(semid, i, NumPlace));
+	}
 
-    for (int i = 0; i <= NumPeople; i++){
-        wait(NULL);
-    }
+	for (int i = 0; i <= NumPeople; i++){
+		wait(NULL);
+	}
 
-    if (semctl(semid, 0 , IPC_RMID, 0) < 0){
-        perror("Cannot delete semid\n");
-        return errno;
-    }
+	if (semctl(semid, 0 , IPC_RMID, 0) < 0){
+		perror("Cannot delete semid\n");
+		return errno;
+	}
 
-    return 0;
+	return 0;
+}
+
+
+int check_end(int semid)
+{
+	struct sembuf check = {SEM_EXIT, 0, IPC_NOWAIT};
+	int res = semop(semid, &check, 1);
+	if (res == -1 && errno == EAGAIN)
+		return 1;
+
+	return 0;
 }
